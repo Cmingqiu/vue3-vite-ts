aa0d6c5e4f7a6a66573e90f533d9817f8115095a
chenmq <1075124372@qq.com>
2022-05-01 16:58:10 +0800

    在生产环境使用mock

--- mock/mockProdServer.ts
+++ mock/mockProdServer.ts
@@ -1,6 +1,7 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
 import mockRoutes from './mock-routes'
+import mockApi from './api'
 
 export function setupProdMockServer() {
-  createProdMockServer([...mockRoutes])
+  createProdMockServer([...mockApi, ...mockRoutes])
 }
